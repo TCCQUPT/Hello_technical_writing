999fc41513683fcab2445f3f065ce981da5f0aea
RobertCheng-956 <RobertCheng-956@users.noreply.github.com>
2025-04-20 09:06:58 +0000

    deploy: 448faa693191bea7c1ab25e5b16595ef55a271ed

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
   },
   {
     "name": "NaNa",
-    "school": "CQUPT!!!!!!!!!",
+    "school": "CQUPT",
     "slogan": "Love words and codes!"
   },
   {
